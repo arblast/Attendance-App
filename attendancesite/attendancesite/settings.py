b26fd8c50237a2b2a3849f32a86eb1b6a3ff9cd2
Justice Yen <justice.yen@gmail.com>
2017-04-20 00:09:44 -0700

    setup postgresql db

--- attendancesite/attendancesite/settings.py
+++ attendancesite/attendancesite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'attendancesite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'attendanceapp',
+        'USER': 'attendanceappuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
