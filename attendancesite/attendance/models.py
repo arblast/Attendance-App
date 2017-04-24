3ffde19312148cb1b66e0573633c03c3ca8465d3
Justice Yen <justice.yen@gmail.com>
2017-04-23 21:54:29 -0700

    add __str__ method

--- attendancesite/attendance/models.py
+++ attendancesite/attendance/models.py
@@ -10,3 +10,6 @@ from django.db import models
 class Attendee(models.Model):
     first_name = models.CharField(max_length = 200)
     last_name = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
