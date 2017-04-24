763048618f745ecddb5f608cd140473390ffdef1
Justice Yen <justice.yen@gmail.com>
2017-04-23 22:11:48 -0700

    add detail view

--- attendancesite/attendance/views.py
+++ attendancesite/attendance/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index.")
+
+def detail(request, attendee_id):
+    return HttpResponse("You're looking at attendee %s" % attendee_id)
