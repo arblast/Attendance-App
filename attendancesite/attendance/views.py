ff8631b4e95c0bdf92c180f14ba9dd432a984708
Justice Yen <justice.yen@gmail.com>
2017-04-23 22:57:20 -0700

    add templates for attendee index and detail

--- attendancesite/attendance/views.py
+++ attendancesite/attendance/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Attendee
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    attendee_list = Attendee.objects.all()
+    context = {'attendee_list': attendee_list}
+    return render(request, 'attendance/index.html', context)
 
 def detail(request, attendee_id):
-    return HttpResponse("You're looking at attendee %s" % attendee_id)
+    attendee = get_object_or_404(Attendee, id = attendee_id)
+    context = {'attendee': attendee}
+    return render(request, 'attendance/detail.html', context)
