e651d65a0b1d3092416244c2073797ac80847707
Justice Yen <justice.yen@gmail.com>
2017-04-26 23:40:15 -0700

    add and migrate groups for attendees

--- attendancesite/attendance/models.py
+++ attendancesite/attendance/models.py
@@ -6,8 +6,16 @@ from django.db import models
 
 
 class Attendee(models.Model):
+    GROUP_NAMES = (
+        ('C', 'Cubbies'),
+        ('S', 'Sparks'),
+        ('T', 'TNT'),
+        ('Tr', 'Trek'),
+        ('J', 'Journey'),
+    )
     first_name = models.CharField(max_length = 200)
     last_name = models.CharField(max_length = 200)
+    group = models.CharField(max_length = 2, choices=GROUP_NAMES)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
