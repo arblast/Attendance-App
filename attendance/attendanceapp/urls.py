0f35e3cb7989a9c78ffbd1ec7c4e07debd1cf48a
Justice Yen <justice.yen@gmail.com>
2017-07-07 21:43:07 -0700

    add more views and urls

--- attendance/attendanceapp/urls.py
+++ attendance/attendanceapp/urls.py
@@ -3,4 +3,6 @@ from attendanceapp import views
 
 urlpatterns = [
     url(r'^hello/', views.hello, name='hello'),
+    url(r'article/(\d+)/', views.viewArticle, name='article'),
+    url(r'articles/(\d{2})/(\d{4})', views.viewArticles, name='articles'),
 ]
--- attendance/attendanceapp/views.py
+++ attendance/attendanceapp/views.py
@@ -12,3 +12,11 @@ from django.http import HttpResponse
 def hello(request):
     text= """<h1>Welcome to my app!</h1>"""
     return HttpResponse(text)
+
+def viewArticle(request, articleId):
+   text = "Displaying article Number : %s"%articleId
+   return HttpResponse(text)
+
+def viewArticles(request, month, year):
+   text = "Displaying articles of : %s/%s"%(year, month)
+   return HttpResponse(text)
