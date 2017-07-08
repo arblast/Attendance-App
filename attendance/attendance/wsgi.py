b62e0f4c1539eea8a02b333049e0b9d7d49a55f0
Justice Yen <justice.yen@gmail.com>
2017-07-07 20:31:24 -0700

    change app structure and name

--- attendance/attendance/wsgi.py
+++ attendance/attendance/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for attendancesite project.
+WSGI config for attendance project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "attendancesite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "attendance.settings")
 
 application = get_wsgi_application()
