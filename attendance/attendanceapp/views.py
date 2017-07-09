e2d0419ef99e8ce68e0690af169d2dbede7bbf34
Justice Yen <justice.yen@gmail.com>
2017-07-08 20:17:11 -0700

    add arguments for url and view to test

--- attendance/attendanceapp/views.py
+++ attendance/attendanceapp/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
@@ -13,12 +13,12 @@ import datetime
 
 def hello(request):
     today = datetime.datetime.now().date()
-    return render(request, "hello.html", {"today": today})
+    return redirect("https://www.djangoproject.com")
 
 def viewArticle(request, articleId):
    text = "Displaying article Number : %s"%articleId
-   return HttpResponse(text)
+   return redirect("articles", year = "2045", month = "02")
 
-def viewArticles(request, month, year):
+def viewArticles(request, year, month):
    text = "Displaying articles of : %s/%s"%(year, month)
    return HttpResponse(text)
