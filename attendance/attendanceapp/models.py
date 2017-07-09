19dac313b95aa8a166f1b812fa75a6571ea0d47a
Justice Yen <justice.yen@gmail.com>
2017-07-08 19:18:57 -0700

    create attendee model

--- attendance/attendanceapp/models.py
+++ attendance/attendanceapp/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Attendee(models.Model):
+
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    club = models.CharField(max_length = 7)
+    
