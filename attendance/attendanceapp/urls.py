4652f2f57cc73878db9d96356d61e89e097ab924
Justice Yen <justice.yen@gmail.com>
2017-07-16 23:19:18 -0700

    add url paths for views

--- attendance/attendanceapp/urls.py
+++ attendance/attendanceapp/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from attendanceapp import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^hello/', views.hello, name='hello'),
     url(r'article/(\d+)/', views.viewArticle, name='article'),
     url(r'articles/(?P<month>\d{2})/(?P<year>\d{4})', views.viewArticles, name='articles'),
+    url(r'^connection/', TemplateView.as_view(template_name = 'login.html')),
+    url(r'^login/', views.login, name = 'login'),
 ]
