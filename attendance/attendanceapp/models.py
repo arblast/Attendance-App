a651cd6bab4f87a4c2f1eb99dc56ac584b85ffe6
Justice Yen <justice.yen@gmail.com>
2017-07-16 22:48:39 -0700

    create date and attendeddates models

--- attendance/attendanceapp/models.py
+++ attendance/attendanceapp/models.py
@@ -2,12 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import datetime
 
 # Create your models here.
 
 class Attendee(models.Model):
-
     first_name = models.CharField(max_length = 50)
     last_name = models.CharField(max_length = 50)
     club = models.CharField(max_length = 7)
-    
+
+class Date(models.Model):
+    date = datetime.date
+
+class AttendedDates(models.Model):
+    attendee = models.ForeignKey(Attendee, on_delete=models.CASCADE)
+    date = models.ForeignKey(Date, on_delete=models.CASCADE)
