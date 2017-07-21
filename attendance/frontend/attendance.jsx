0fc42ef5add7c0be5c5020f933f2a45670896a85
Justice Yen <justice.yen@gmail.com>
2017-07-20 22:19:14 -0700

    complete react setup

--- attendance/frontend/attendance.jsx
+++ attendance/frontend/attendance.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let Hello = React.createClass ({
-    render: function() {
+let Hello = () => {
         return (
             <h1>
             Hello, React!
             </h1>
-        )
-    }
-})
+          )
+        };
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
