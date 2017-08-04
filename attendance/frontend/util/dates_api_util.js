93e66200493d3966352a218e2d62d1f587cf6d13
Justice Yen <justice.yen@gmail.com>
2017-08-03 20:57:19 -0700

    complete utils for dates

--- attendance/frontend/util/dates_api_util.js
+++ attendance/frontend/util/dates_api_util.js
@@ -8,3 +8,13 @@ export const createDate = (dateParams, token, success, error) => {
     error
   });
 }
+
+export const fetchDate = (dateParams, success, error) => {
+  $.ajax({
+    type: "GET",
+    data: dateParams,
+    url: "/attendanceapp/date/",
+    success,
+    error
+  });
+};
